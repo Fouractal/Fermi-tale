0e1316b51e88f534d7452070d9949cf0a5067fff
aloHyomora <alohomoratolit@naver.com>
2023-06-17 19:31:30 +0900

    feat : Joystick Input 플레이어 이동 좌표로 변환

--- Assets/DevOnly/StreetHyo/PlayerController.cs
+++ Assets/DevOnly/StreetHyo/PlayerController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class sampleMove : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody _rigidbody;
+
+    public Vector2 direction;
+    private float _angle = -45f; //회전할 각도 (단위: 도)
+    private float _radians = 0f; // 각도를 라디안으로 변환
     void Start()
     {
+        _radians = _angle * Mathf.Deg2Rad;
         _rigidbody = GetComponent<Rigidbody>();
     }
 
@@ -15,9 +20,29 @@ public class sampleMove : MonoBehaviour
     {
         Move();
     }
+    public Vector3 ConvertXYtoXZ()
+    {
+        float x = direction.x;
+        float y = direction.y;
 
+        float newX = x * Mathf.Cos(_radians) - y * Mathf.Sin(_radians);
+        float newZ = x * Mathf.Sin(_radians) + y * Mathf.Cos(_radians);
+
+        return new Vector3(newX, 0f, newZ);
+    }
     void Move()
     {
+        // (x, y) direction Vector -> (45도 회전된) XZ 평면에서의 방향으로 변환
+        _rigidbody.position += ConvertXYtoXZ() * speed * Time.fixedDeltaTime;
+        
+
+
+        // (1,0)  -> (Vector3.back + Vector3.right) * speed
+        // (-1,0) -> (Vector3.forward + Vector3.left) * speed
+        // (0,1)  -> (Vector3.forward + Vector3.right) * speed
+        // (0,-1) -> (Vector3.back + Vector3.left) * speed
+        
+        
         // 45도 씩 돌려서 이동
         if (Input.GetKey(KeyCode.W))
         {
