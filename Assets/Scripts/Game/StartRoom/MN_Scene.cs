fcd4aff13a107b0957731dc47f278cc29ea84f89
aloHyomora <alohomoratolit@naver.com>
2023-07-22 12:07:51 +0900

    feat : assign Cinemachine Virtual Cam

--- Assets/Scripts/Game/StartRoom/MN_Scene.cs
+++ Assets/Scripts/Game/StartRoom/MN_Scene.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,10 +14,18 @@ public class MN_Scene : MonoBehaviour
         
         //캐릭터 생성
         GameObject mainCharacterPrefab = Resources.Load<GameObject>("Prefabs/MainCharacter/MainCharacter_Prefab");
-        PlayerCharacterManager.Instance.playerController = Instantiate(mainCharacterPrefab).GetComponent<PlayerController>();
+        GameObject playerObject = Instantiate(mainCharacterPrefab);
+        PlayerCharacterManager.Instance.playerController = playerObject.GetComponent<PlayerController>();
         
+        // Cinemachine Virtual Cam에 Player 할당, 근데 씬 생성될 때 동적으로 virtualCam 컴포넌트 싱글톤으로 생성됨 근데 다시 할당한다?
+        CinemachineVirtualCamManager.Instance.virtualCamera = GameObject.Find("followCam").GetComponent<CinemachineVirtualCamera>();
+        CinemachineVirtualCamManager.Instance.virtualCamera.LookAt = playerObject.transform;
+        CinemachineVirtualCamManager.Instance.virtualCamera.Follow = playerObject.transform;
+            
         //조이스틱 생성
         GameObject joystickUICanvasPrefab = Resources.Load<GameObject>("Prefabs/UI/JoystickUICanvas");
         Instantiate(joystickUICanvasPrefab);
+        
+        
     }
 }
