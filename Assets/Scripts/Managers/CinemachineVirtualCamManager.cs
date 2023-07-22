0c6ddbe5b4e27815bf1e218bf34567083b387ec9
aloHyomora <alohomoratolit@naver.com>
2023-07-22 12:07:51 +0900

    feat : assign Cinemachine Virtual Cam

---
+++ Assets/Scripts/Managers/CinemachineVirtualCamManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CinemachineVirtualCamManager : Singleton<CinemachineVirtualCamManager>
+{
+    public CinemachineVirtualCamera virtualCamera;
+}
