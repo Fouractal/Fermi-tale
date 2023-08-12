22b429dcceadd563f215f5c0b4db8566ccf650da
aloHyomora <alohomoratolit@naver.com>
2023-08-12 11:23:48 +0900

    feat ; player animation

--- Assets/Scripts/UI/JoystickUICanvas.cs
+++ Assets/Scripts/UI/JoystickUICanvas.cs
@@ -18,7 +18,7 @@ public class JoystickUICanvas : MonoBehaviour
     private Vector2 endPos;         // 끝점 위치
 
     private Vector2 direction;      // 계산한 방향
-
+    private float _moveBlendValue;
     private void Awake()
     {
         _playerController = PlayerCharacterManager.Instance.playerController;
@@ -42,11 +42,19 @@ public class JoystickUICanvas : MonoBehaviour
     private void CharacterMove(PointerEventData eventData)
     {
         // 플레이어가 이동할 "방향" 업데이트
-        //Debug.Log($"CharacterMove");
         endPos = eventData.position;
+        // JoyStick 터치 길이에 따른 Move Blend Tree 값 변경, (Idle <-> Walk <-> Run)
+        Debug.Log(Mathf.Abs(Mathf.Pow(endPos.x - startPos.x,2)
+                            + Mathf.Pow(endPos.y - startPos.y,2)));
+        //임의로 20만을 Blend Tree 최댓값으로 설정.
+        
         _playerController.direction = (endPos - startPos).normalized;
-        _playerController.playerAnimator.SetBool("IsWalking",true);
-        Debug.Log(_playerController.direction);
+        _moveBlendValue = (Mathf.Abs(Mathf.Pow(endPos.x - startPos.x, 2)
+                                     + Mathf.Pow(endPos.y - startPos.y, 2))) / 100000f;
+        
+        _playerController.playerAnimator.SetFloat("MoveBlend", _moveBlendValue);
+        //_playerController.playerAnimator.SetBool("IsWalking",true);
+        //Debug.Log(_playerController.direction);
     }
 
     private void AfterInteraction(PointerEventData eventData)
@@ -54,6 +62,6 @@ public class JoystickUICanvas : MonoBehaviour
         // 이동 종료시 direction 제거
         Debug.Log($"AfterInteraction");
         _playerController.direction = Vector2.zero;
-        _playerController.playerAnimator.SetBool("IsWalking",false);
+        //_playerController.playerAnimator.SetBool("IsWalking",false);
     }
 }
