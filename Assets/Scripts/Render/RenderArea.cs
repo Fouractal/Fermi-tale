656d3b98e328abb59f4d06b4146212abeab93488
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-10-07 16:58:31 +0900

    chore : build settings

--- Assets/Scripts/Render/RenderArea.cs
+++ Assets/Scripts/Render/RenderArea.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class RenderArea : Singleton<RenderArea>
