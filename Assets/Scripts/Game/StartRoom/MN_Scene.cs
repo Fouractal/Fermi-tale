fb9510002501ce8748a7a9431aeea0fba4465479
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-10-07 15:54:25 +0900

    chore : MN_Scene에 PlayerController, CameraTurnController 반영

--- Assets/Scripts/Game/StartRoom/MN_Scene.cs
+++ Assets/Scripts/Game/StartRoom/MN_Scene.cs
@@ -18,8 +18,6 @@ public class MN_Scene : MonoBehaviour
         PlayerCharacterManager.Instance.player = playerObject.GetComponent<Player>();
         
         // Cinemachine Virtual Cam에 Player 할당, 근데 씬 생성될 때 동적으로 virtualCam 컴포넌트 싱글톤으로 생성됨 근데 다시 할당한다?
-        var cameraRoot = GameObject.Find("CameraRoot");
-        CinemachineVirtualCamManager.Instance.cameraRoot = cameraRoot;
         //cameraRoot.transform.SetParent();
         CinemachineVirtualCamManager.Instance.virtualCamera = GameObject.Find("followCam").GetComponent<CinemachineVirtualCamera>();
         CinemachineVirtualCamManager.Instance.virtualCamera.LookAt = playerObject.transform;
