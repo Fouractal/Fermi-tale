f61a6a92deecc5ab4eeeb1e43cf201c965ef7edb
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-11-19 22:28:28 +0900

    feat : long press (1sec)

---
+++ Assets/Scripts/Utils/LongPress.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public delegate void PressHandler(PointerEventData eventData);
+
+    public event PressHandler OnLongPress;
+
+    public float pressThreshold;
+
+    private Coroutine _pressCheckRoutine = null;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _pressCheckRoutine = StartCoroutine(PressCheckRoutine(eventData));
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        StopCoroutine(_pressCheckRoutine);
+    }
+
+    private IEnumerator PressCheckRoutine(PointerEventData eventData)
+    {
+        yield return new WaitForSecondsRealtime(pressThreshold);
+        OnLongPress?.Invoke(eventData);
+    }
+}
