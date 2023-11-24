cea683e917d75b903f6f07554ff2ce02352ac6c7
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-11-25 01:46:54 +0900

    feat : 시계 Ticktock 기능 구현 (시곗바늘이 멈췄다가 움직이는 기능)

--- Assets/Scripts/Game/FD/HugeClock.cs
+++ Assets/Scripts/Game/FD/HugeClock.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HugeClock : MonoBehaviour
 {
@@ -10,25 +12,36 @@ public class HugeClock : MonoBehaviour
     public GameObject minuteHand;
 
     [Header("Data")]
-    public float speed;
     public float areaSize;
     public AudioSource audioSource;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
-    }
-    
-    public void Update()
-    {
-        minuteHand.transform.Rotate(Vector3.up * speed / 60 * Time.deltaTime);
-        hourHand.transform.Rotate(Vector3.up * speed / 3600 * Time.deltaTime);
+        StartCoroutine(Ticktock());
     }
 
     public void Alert()
     {
         audioSource.clip = Resources.Load<AudioClip>("Sounds/FouractalClock1");
         audioSource.Play();
+    }
+
+    public IEnumerator Ticktock()
+    {
+        int time = 0;
+
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            time++;
+            time %= 12 * 12;
+            
+            Sequence sequence = DOTween.Sequence();
+            sequence
+                .Append(minuteHand.transform.DOLocalRotate(Vector3.up * time * 30, 0.5f).SetEase(Ease.OutCubic))
+                .Join(hourHand.transform.DOLocalRotate(Vector3.up * time * 2.5f, 0.5f).SetEase(Ease.OutCubic));
+        }
     }
     
 }
