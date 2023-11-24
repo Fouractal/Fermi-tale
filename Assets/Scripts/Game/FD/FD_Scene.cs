cd7085cab8aeffd840a37d503556e518685b6269
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-11-25 01:12:50 +0900

    feat : FD_Scene 초기화, 플레이어 생성

---
+++ Assets/Scripts/Game/FD/FD_Scene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class FD_Scene : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject mainCharacterPrefab = Resources.Load<GameObject>("Prefabs/MainCharacter/MainCharacter_Prefab");
+        GameObject playerObject = Instantiate(mainCharacterPrefab);
+        PlayerCharacterManager.Instance.player = playerObject.GetComponent<Player>();
+        
+        CinemachineVirtualCamManager.Instance.virtualCamera = GameObject.Find("followCam").GetComponent<CinemachineVirtualCamera>();
+        CinemachineVirtualCamManager.Instance.virtualCamera.LookAt = playerObject.transform;
+        CinemachineVirtualCamManager.Instance.virtualCamera.Follow = playerObject.transform;
+        
+        GameObject touchPadPrefab = UIManager.Instance.ShowSceneUI("Prefabs/UI/TouchPadCanvas"); 
+    }
+}
