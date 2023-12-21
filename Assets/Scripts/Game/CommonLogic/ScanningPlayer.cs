1d15077787f2f2b549db945f57b69794fc266d1f
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2023-12-22 00:42:48 +0900

    feat : animator, edit script

--- Assets/Scripts/Game/CommonLogic/ScanningPlayer.cs
+++ Assets/Scripts/Game/CommonLogic/ScanningPlayer.cs
@@ -1,20 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening;
-using DG.Tweening.Plugins.Core.PathCore;
 using UnityEngine;
-
-[ExecuteInEditMode] // 편집 모드에서 테스트하기 위해 편집 모드에서 실행 추가
-public class ChasingPlayer : MonoBehaviour
+using DG.Tweening;
+public class ScanningPlayer : MonoBehaviour
 {
-    [Header("Animation property")]
-    [SerializeField] private Animator chasingAnimator;
-    public Transform animationOriginTransform;
-    public bool isChasing = false;
-    [SerializeField] private Vector3 _lastPosition;
-    [SerializeField] private Quaternion _lastRotation;
-    
+    // Player를 실시간 스캔, Animation 재생의 origin 게임 오브젝트에 부착될 스크립트.
+    // 가장 상위 스크립트에 감지된 player 정보 전달함.
     public float distance = 10f;
     public float angle = 30f;
     public float height = 1.0f;
@@ -33,62 +24,18 @@ public class ChasingPlayer : MonoBehaviour
     private DOTweenPath _doTweenPath;
     void Start()
     {
-        _doTweenPath = GetComponent<DOTweenPath>();
         _scanInterval = 1.0f / scanFrequency;
-        
-        _lastPosition = animationOriginTransform.position;
-        _lastRotation = animationOriginTransform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            chasingAnimator.SetBool("IsChasing", true);    
-        }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            chasingAnimator.SetBool("IsChasing", false);    
-        }
-        
-        
         _scanTimer -= Time.deltaTime;
         if (_scanTimer < 0)
         {
             _scanTimer += _scanInterval;
             Scan();
         }
-    }
-
-    
-    // 애니메이션 이벤트, 애니메이션 종료 시점에 호출될 메서드
-    public void OnAnimationEnd()
-    {
-        Debug.Log("Animation end! ");
-        SaveAnimationEndTransform();
-    }
-    // 애니메이션 이벤트, 애니메이션 시작 시점에 호출될 메서드
-    public void OnAnimationStart()
-    {
-        Debug.Log("Animation start! ");
-        ResetPositionToLast();
-    }
-    
-    
-    // 애니메이션 종료 시점에 위치, 회전값을 기억
-
-    public void SaveAnimationEndTransform()
-    {
-        _lastPosition = animationOriginTransform.position;
-        _lastRotation = animationOriginTransform.rotation;
-    }
-    // 애니메이션 시작 시점에 호출될 메서드
-    public void ResetPositionToLast()
-    {
-        transform.position = new Vector3(_lastPosition.x, 0.5f, _lastPosition.z);
-        transform.rotation = _lastRotation;
-        
     }
     
     private void Scan()
