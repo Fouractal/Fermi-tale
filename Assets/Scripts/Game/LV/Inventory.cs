32fd478bac86ed8aea6c88077bc39024e111631f
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-23 12:08:24 +0900

    feat : simple inventory

--- Assets/Scripts/Game/LV/Inventory.cs
+++ Assets/Scripts/Game/LV/Inventory.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static IInteractable _item;
+    
+    public static void RegisterItem(IInteractable item)
     {
-        
+        _item = item;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void DeregisterItem()
     {
-        
+        _item = null;
     }
 }
