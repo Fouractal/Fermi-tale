bf3dd43a119e3d5835271f8d745580bfdc7c29d7
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-23 11:55:50 +0900

    feat : LV Game rule
    - Timer System
    - 장미 Quest Routine
    - 장미 Qeust UI
    - Item Interaction
    - Game Routine

--- Assets/Scripts/Game/LV/LV_Scene.cs
+++ Assets/Scripts/Game/LV/LV_Scene.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Managers.GameManage;
 
-public class LV_Scene : MonoBehaviour
+namespace Game.LV
 {
-    private void Awake()
+    public class LV_Scene : GameScene
     {
-        GameObject mainCharacterPrefab = Resources.Load<GameObject>("Prefabs/MainCharacter/MainCharacter_Prefab");
-        GameObject playerObject = Instantiate(mainCharacterPrefab, Vector3.up * 5, Quaternion.identity);
-        playerObject.transform.SetParent(GameObject.Find("ObjectRoot").transform);
-        PlayerCharacterManager.Instance.player = playerObject.GetComponent<Player>();
+        private void Awake()
+        {
+            GameObject mainCharacterPrefab = Resources.Load<GameObject>("Prefabs/MainCharacter/MainCharacter_Prefab");
+            GameObject playerObject = Instantiate(mainCharacterPrefab, Vector3.up * 5, Quaternion.identity);
+            playerObject.transform.SetParent(GameObject.Find("ObjectRoot").transform);
+            PlayerCharacterManager.Instance.player = playerObject.GetComponent<Player>();
         
-        CinemachineVirtualCamManager.Instance.virtualCamera = GameObject.Find("followCam").GetComponent<CinemachineVirtualCamera>();
-        CinemachineVirtualCamManager.Instance.cinemachineOrbitalTransposer = CinemachineVirtualCamManager.Instance.virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
-        CinemachineVirtualCamManager.Instance.virtualCamera.LookAt = playerObject.transform;
-        CinemachineVirtualCamManager.Instance.virtualCamera.Follow = playerObject.transform;
+            CinemachineVirtualCamManager.Instance.virtualCamera = GameObject.Find("followCam").GetComponent<CinemachineVirtualCamera>();
+            CinemachineVirtualCamManager.Instance.cinemachineOrbitalTransposer = CinemachineVirtualCamManager.Instance.virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+            CinemachineVirtualCamManager.Instance.virtualCamera.LookAt = playerObject.transform;
+            CinemachineVirtualCamManager.Instance.virtualCamera.Follow = playerObject.transform;
         
-        GameObject touchPadPrefab = UIManager.Instance.ShowSceneUI("Prefabs/UI/TouchPadCanvas");
+            GameObject touchPadPrefab = UIManager.Instance.ShowSceneUI("Prefabs/UI/TouchPadCanvas");
+            GameManager.Instance.GameScene = this;
+        }
+
+        public void GenerateRoseQuestUI(Rose rose)
+        {
+        
+        }
     }
+    
 }
