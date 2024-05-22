63399b86835b78790c9c09e9515df6b6a0898c34
kkkkiiiiii <alohomoratolit@naver.com>
2024-05-23 00:26:22 +0900

    Add : FM 폴더, FM_GameFramework, FM_Scene
    namespace Game.FM 아래
    - FM_GameFramework, FM_Scene 코드 생성
    - Define -> Phase

---
+++ Assets/Scripts/Game/FM/FM_Scene.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Managers.GameManage;
+using UnityEngine;
+
+namespace Game.FM
+{
+    public class FM_Scene : GameScene
+    {
+        private void Awake()
+        {
+            // TODO : 씬 초기화 코드
+        }
+    }
+}
+
