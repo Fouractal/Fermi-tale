d4efdf0f15edc729e1cfb868c4aafb51097c03d0
kkkkiiiiii <alohomoratolit@naver.com>
2024-05-24 23:39:17 +0900

    feat : FM 기본 로직

--- Assets/Scripts/Game/FM/FM_GameFramework.cs
+++ Assets/Scripts/Game/FM/FM_GameFramework.cs
@@ -6,9 +6,14 @@ namespace Game.FM
 {
     public class FM_GameFramework : GameFramework
     {
+        public Phase phase = Phase.ConversationOne;
+
+        public int successNumber;
+        
         // Start is called before the first frame update
         void Start()
         {
+            phase = Phase.ConversationOne;
             StartCoroutine(GameFramework());
         }
 
@@ -19,7 +24,22 @@ namespace Game.FM
             // 특정 대화와 일치하는 아이템이 맵에 존재
             // 해당 아이템 찾으면 해당 주제 클리어
             // 총 세 가지 대화 클리어 후 마지막 대화 주제가 남음
+
+            yield return new WaitUntil(() => phase == Phase.ConversationOne);
+            
+            yield return new WaitUntil(() => phase == Phase.ConversationTwo);
+
+            yield return new WaitUntil(() => phase == Phase.ConversationThree);
+
+            yield return new WaitUntil(() => phase == Phase.ConversationLast);
+
             yield break;
+        }
+
+        public void Success()
+        {
+            Debug.Log("Success " +phase);
+            phase++;
         }
     }
 }
