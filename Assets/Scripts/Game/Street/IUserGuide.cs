67ade7d4de7bf505c0b1c7796b6519b605973c4e
kkkkiiiiii <alohomoratolit@naver.com>
2024-09-21 11:40:07 +0900

    [Feat] : IUserGUide 인터페이스
    - 튜토리얼 각 요소가 포함해야 할 메서드

---
+++ Assets/Scripts/Game/Street/IUserGuide.cs
@@ -0,0 +1,15 @@
+public interface IUserGuide
+{
+    void ShowInstructions();  // 사용법 안내를 화면에 보여주는 메서드
+
+    void PlayEffect(); // 연출 효과를 실행하는 메서드
+
+    void StopEffect(); // 연출을 멈추는 메서드
+    
+    bool IsGuideCompleted();  // 사용자가 안내를 모두 완료했는지 확인하는 메서드
+}
+
+
+// TODO : 추가로 구현하면 좋을 것?
+
+// void ShowHint(string context);  // 특정 상황에 맞는 힌트를 제공하는 메서드
