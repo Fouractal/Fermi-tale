ad0e629fec2e3ae401dff5c4ab919a63044334a5
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-09-28 13:37:01 +0900

    refactor : camera init

--- Assets/Scripts/Game/Family/FamilyScene.cs
+++ Assets/Scripts/Game/Family/FamilyScene.cs
@@ -17,12 +17,14 @@ namespace Game.FM
             GameObject playerObject = Instantiate(mainCharacterPrefab, Vector3.up * 5, Quaternion.identity);
             // playerObject.transform.SetParent(GameObject.Find("ObjectRoot").transform);
             PlayerManager.Instance.player = playerObject.GetComponent<Player>();
-        
-            CameraManager.Instance.virtualCamera = GameObject.Find("followCam").GetComponent<CinemachineVirtualCamera>();
+
+            GameObject virtualCam = GameObject.Find("followCam");
+            CameraManager.Instance.fermiCamera = virtualCam.GetComponent<FermiCamera>();
+            CameraManager.Instance.virtualCamera = virtualCam.GetComponent<CinemachineVirtualCamera>();
             CameraManager.Instance.cinemachineOrbitalTransposer = CameraManager.Instance.virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
             CameraManager.Instance.virtualCamera.LookAt = playerObject.transform;
             CameraManager.Instance.virtualCamera.Follow = playerObject.transform;
-        
+
             GameObject touchPadPrefab = UIManager.Instance.ShowSceneUI("Prefabs/UI/TouchPadCanvas");
             GameManager.Instance.GameScene = this;
         }
--- Assets/Scripts/Game/Friend/FriendScene.cs
+++ Assets/Scripts/Game/Friend/FriendScene.cs
@@ -9,16 +9,18 @@ namespace Game.FD
         protected override void Awake()
         {
             base.Awake();
-            
+
             GameObject mainCharacterPrefab = Resources.Load<GameObject>("Prefabs/MainCharacter/MainCharacter_Prefab");
             GameObject playerObject = Instantiate(mainCharacterPrefab);
             PlayerManager.Instance.player = playerObject.GetComponent<Player>();
-        
-            CameraManager.Instance.virtualCamera = GameObject.Find("followCam").GetComponent<CinemachineVirtualCamera>();
+
+            GameObject virtualCam = GameObject.Find("followCam");
+            CameraManager.Instance.fermiCamera = virtualCam.GetComponent<FermiCamera>();
+            CameraManager.Instance.virtualCamera = virtualCam.GetComponent<CinemachineVirtualCamera>();
             CameraManager.Instance.cinemachineOrbitalTransposer = CameraManager.Instance.virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
             CameraManager.Instance.virtualCamera.LookAt = playerObject.transform;
             CameraManager.Instance.virtualCamera.Follow = playerObject.transform;
-        
+
             GameObject touchPadPrefab = UIManager.Instance.ShowSceneUI("Prefabs/UI/TouchPadCanvas");
         }
     }
