9477bd2663f21d26fb8e699d8c07449ca1fd095b
kkkkiiiiii <alohomoratolit@naver.com>
2024-11-01 22:42:18 +0900

    #63 [Add] : auto property

--- Assets/Scripts/Game/Street/BaseInteractive.cs
+++ Assets/Scripts/Game/Street/BaseInteractive.cs
@@ -4,12 +4,11 @@ public abstract class BaseInteractive : MonoBehaviour
 {
     public abstract void InitializeGuide(); // 각 Phase가 시작할 시점에 변수 초기화하는 메서드
     public abstract void ShowInstructions();  // 사용법 안내를 화면에 보여주는 메서드
-
     public abstract void PlayEffect(); // 연출 효과를 실행하는 메서드
-
     public abstract void StopEffect(); // 연출을 멈추는 메서드
-    
-    public abstract bool CheckIsGuideCompleted();  // 사용자가 안내를 모두 완료했는지 확인하는 메서드
+
+    // auto property
+    public bool IsCompleted {get;set;} = false;
 }
 // TODO : 추가로 구현하면 좋을 것?
 
