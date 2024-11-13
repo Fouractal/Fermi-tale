2f3343b5b0c913bf1d54d231ce037eaaf1993778
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-11-14 07:34:36 +0900

    fix : render by direction 적용 안되는 현상

--- Assets/Scripts/Managers/CamManage/FermiCamera.cs
+++ Assets/Scripts/Managers/CamManage/FermiCamera.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class FermiCamera : MonoBehaviour
 {
-    public TouchPad touchPad;
     public bool isTurning = false;
 
     [SerializeField] private CinemachineOrbitalTransposer orbitalTransposer;
@@ -15,19 +14,14 @@ public class FermiCamera : MonoBehaviour
     public delegate void DirectionHandler(Define.CameraDirection nextDirection);
     public event DirectionHandler OnChangeDirection;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        touchPad = TouchPad.Instance;
-        // touchPad.doubleTap.OnDoubleTap += CameraTurn;
-
         orbitalTransposer = CameraManager.Instance.cinemachineOrbitalTransposer;
         orbitalTransposer.m_RecenterToTargetHeading.m_enabled = true;
         orbitalTransposer.m_Heading.m_Definition = CinemachineOrbitalTransposer.Heading.HeadingDefinition.WorldForward;
         orbitalTransposer.m_Heading.m_Bias = 45;
         orbitalTransposer.m_XAxis.Value = 0;
     }
-    
     
     public void CameraTurnClockwise()
     {
@@ -36,7 +30,6 @@ public class FermiCamera : MonoBehaviour
 
         cameraDirection = (Define.CameraDirection)(((int)cameraDirection + 1) % 4);
         OnChangeDirection?.Invoke(cameraDirection);
-        
     }
 
     public void CameraTurnCounterClockwise()
@@ -46,6 +39,5 @@ public class FermiCamera : MonoBehaviour
         
         cameraDirection = (Define.CameraDirection)(((int)cameraDirection + 3) % 4);
         OnChangeDirection?.Invoke(cameraDirection);
-        
     }
 }
